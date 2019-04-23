1bac505ba8e0e64f38b203c93fc39c46a26387ee
Kei Yoshida <kyoshida@coe.edu>
2019-04-23 09:32:45 -0500

    Frequent commit

--- cpmcopy.c
+++ cpmcopy.c
@@ -3,16 +3,57 @@
 #include "initializeDisk.h"
 #include "directory.h"
 
-/*  command line:
-      cpmcopy -f/h fromDisk toDisk fileName1 fileName2 ...
-*/
+#define USAGE "Usage: ./cpmcopy [-fd] [-io] diskname filename(s)"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv[]) {
+  int diskTypeFlag, inoutFlag;  // diskTypeFlag: 0=floppy, 1=hard disk
+                                // inoutFlag: 0=in (local to disk), 1=out (disk to local)
 
-  if (argc<5){
-    printf("Usage: ./cpmcopy -f/h fromDisk toDisk fileName1 fileName2 ...\n");
-    return -1;
+  while ((opt = getopt(argc, argv, "fdio")) != -1) {
+    switch (opt) {
+      case 'f':
+        diskTypeFlag = 0;
+        break;
+      case 'd':
+        diskTypeFlag = 1;
+        break;
+      case 'i':
+        inoutFlag = 0;
+      case 'o':
+        inoutFlag = 1;
+      default:
+        printf("%s\n", usage);
+        exit(-1);
+    }
   }
+
+  FILE* diskIn, diskOut;
+  if ((diskIn = fopen(argv[2], "r")) == NULL) {
+		printf("Unable to open the disk %s\n", argv[2]);
+		return -1;
+	}
+  if ((diskOut = fopen(argv[3], "r+")) == NULL) {
+		printf("Unable to open the disk %s\n", argv[3]);
+		return -1;
+  }
+
+  int index = 4;
+  FILE *fp;
+  while (index < argc) {
+    if ((fp = fopen(argv[index], "r")) == NULL) {
+      printf("%s can't open file %s\n", argv[0], argv[index]);
+      return 1;
+    }
+    else {
+      while (fgets(line, MAX_LINE, fp) != NULL) {
+        numlines++;
+        numchars += mygetchars(line);
+        numwords += mygetwords(line);
+      }
+    }
+    index++;
+  }
+
 
   struct directory d;
 
--- directory.h
+++ directory.h
@@ -20,13 +20,12 @@ struct disk
 
 struct directory {
   unsigned char userNum; // 0 or E5 Status Byte. Read
-  char fileName[8];
-  char fileType[3];
+  char fileName[7];
+  char fileType[2];
   unsigned char fileExtent;
-  unsigned char reserve1;
-  unsigned char reserve2;
+  unsigned char reserve[1];
   unsigned char recordNum;
-  char diskmap[16];
+  char diskmap[15];
 };
 
 struct directoryList {
