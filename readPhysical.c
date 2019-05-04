92d2f6e2cdbca51707fec423ff29cb807b2fa91e
Julieon1 <rjpeterson@coe.edu>
2019-05-04 18:07:56 -0500

    Removed readLogical Dependency

--- readPhysical.c
+++ readPhysical.c
@@ -5,14 +5,7 @@
 #include "readPhysical.h"
 
 
-void readPhysical(FILE* diskIn, int offset, bytePtr sector, int flag ) {
+void readPhysical(FILE* diskIn, int offset, bytePtr sector, int flag, bytePtr block ) {
   fseek(diskIn, offset, SEEK_SET);
   fread(sector, sizeof(sector), 1, diskIn);
-  /*
-  for (int i = 0 ; i < 128 ; i++) {
-    printf("%x", sector[i]);
-    printf("%s", " ");
-  }
-  */
-
 }
