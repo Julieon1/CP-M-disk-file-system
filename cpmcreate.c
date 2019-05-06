17104ee67c47885d967c1619a3e6a4f375f4179b
terrencehall56 <tmhall@coe.edu>
2019-05-06 17:21:59 -0500

    cpmCreate Functionality

--- cpmcreate.c
+++ cpmcreate.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/*  command line:
-      cpmcreate ____.disk
-*/
+#include "disk.h"
+#include "directory.h"
+#include "readFlag.h"
 
 int main(int argc, char** argv) {
 
+
+  int flag = readFlag(argc, argv); // 1 = floppy , 0 = hard disk
+  bytePtr byte = malloc(1);
+  byte = 0xe5;
   FILE *outfile;
-	outfile = fopen(argv[1], "wb");
-  
+	outfile = fopen(argv[2], "r+");
+
+    for (i=0; i < (seclen * tracks[flag] * sectrk[flag]); i++ ){
+      fwrite(byte, sizeof(byte), 1, outfile);
+    }
+
 
   fclose(outfile);
 }
